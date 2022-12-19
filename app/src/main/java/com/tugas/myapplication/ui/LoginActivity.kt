b8630eae47b1561629cf0d056e62d7895c1994c7
mansao01 <novrilukman15@gmail.com>
2022-12-19 21:40:07 +0700

    working on activity_login.xml

--- app/src/main/java/com/tugas/myapplication/ui/LoginActivity.kt
+++ app/src/main/java/com/tugas/myapplication/ui/LoginActivity.kt
@@ -1,12 +1,14 @@
 package com.tugas.myapplication.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.tugas.myapplication.R
+import androidx.appcompat.app.AppCompatActivity
+import com.tugas.myapplication.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
